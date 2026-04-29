9d4af854f7b60cb8236b6b3629092af3b80f1082
Joao Vitor <joaorabello99@gmail.com>
2026-04-29 01:23:29 -0300

    chore: add .gitignore, remove node_modules and dist from tracking

--- dist/src/adapters/me.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toMe = toMe;
-function toMe(user, student, journey) {
-    return { user, student, journey };
-}
--- dist/src/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildApp = buildApp;
-const me_1 = require("./diplomat/http-server/me");
-function buildApp() {
-    (0, me_1.registerMeRoutes)();
-}
